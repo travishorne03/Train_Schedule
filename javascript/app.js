b0faee371c45962759ee4728349ae81c7c66d3aa
T Horne <travis.horne03@gmail.com>
2017-10-09 20:36:15 -0400

    update

---
+++ javascript/app.js
@@ -0,0 +1,17 @@
+// Doc ready funtion
+
+// Firebase code
+
+//CLear Form function at the bottom of page
+
+//Take the data from the form and pushing it to firebase
+
+//var calculation time
+
+//database.ref().on("child_added".function(snapshot) - adds new row to table
+
+//Delete train button
+
+//Edit train button- child.data
+
+//Play some music at the end 
