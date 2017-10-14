996f6d549db94b949ccd27d8652dace704c459c6
T Horne <travis.horne03@gmail.com>
2017-10-13 20:52:30 -0400

    update

--- javascript/app.js
+++ javascript/app.js
@@ -22,16 +22,31 @@ $("#add-user").on("click",function(event) {
   event.preventDefault();
   var trainName = $("#name-display").val().trim();
   var destination = $("#destination-display").val().trim();
-  var trainTime = $("#start-display").val().trim();
-  var frequency = $("#frequency-display").val().trim();
+  var trainTime = moment($("#start-display").val().trim(), "HH:mm").format("HH:mm a");
+  var frequency = moment($("#frequency-display").val().trim(), "mm").format("mm");
+console.log(frequency);
+  //var calculation time
+  
+  var formated = moment(trainTime, "hh:mm").subtract(1, "years");
+  console.log(formated);
+  var diff = moment().diff(moment(formated), "minutes");
+  console.log(diff);
+  var apart = diff % frequency;
+  console.log(apart);
+  var away = frequency - apart;
+
+  var arrival = moment().add(away, "minutes").format("hh:mm");
 
 //Take the data from the form and pushing it to firebase
+
 
   database.ref().push({
     name: trainName,
     destination: destination,
     time:  trainTime,
-    freq: frequency
+    freq: frequency,
+    arrival: arrival,
+    away: away
     
   });
 
@@ -50,34 +65,44 @@ $("#add-user").on("click",function(event) {
 
 
 
-
-
-//var calculation time
-
-
-
-
 //database.ref().on("child_added".function(snapshot) - adds new row to table
 
-dataRef.ref().on("child_added",function(childSnapshot){
+database.ref().on("child_added",function(childSnapshot, prevChildKey){
 
-console.log(childSnapshot.val().trainName)
+  console.log(prevChildKey);
+
+  var trainName= childSnapshot.val().name;
+  var destination = childSnapshot.val().destination;
+  var trainTime = childSnapshot.val().time;
+  var frequency = childSnapshot.val().frequency;
+  var arrival = childSnapshot.val().arrival;
+ var away = childSnapshot.val().away;
+
+
+console.log(childSnapshot.val().name)
 console.log(childSnapshot.val().destination)
-console.log(childSnapshot.val().trainTime)
-console.log(childSnapshot.val().frequency)
+console.log(childSnapshot.val().time)
+console.log(childSnapshot.val().freq)
 console.log(childSnapshot.val().arrival)
 console.log(childSnapshot.val().away)
 
-newRW = $<"tr">
-$("#dispv").append(newRW);
-newRW.append("<td>" + childSnapshot.val().trainName + "</td>");
+//newRW = $("<tr>")
+$("#dispSV" ).append(
+"<tr><td>" + trainName + 
+"</td><td>" + destination + 
+"</td><td>"+ trainTime + 
+"</td><td>"+ frequency + 
+"</td><td>" + arrival + 
+"</td><td>" + away + "</td></tr>"
 
 
-
-})
+);
 
 
 
 
 //Play some music at the end 
-})
+});
+
+});
+
